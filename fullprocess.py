874809291c04e9dcfa7a9ecfadf8996eab025ac1
Zhongqi Yue <z3yue@uwaterloo.ca>
2022-07-10 13:57:48 -0400

    final

--- fullprocess.py
+++ fullprocess.py
@@ -1,40 +1,71 @@
-
-
 import training
 import scoring
 import deployment
 import diagnostics
 import reporting
+import logging
+from ingestion import merge_multiple_dataframe
+from training import train_model
+from deployment import store_model_into_pickle
+from reporting import score_model
+import pickle
+import json
+import os
+import pandas as pd
+from sklearn import metrics
+logging.basicConfig(level = logging.INFO)
 
-##################Check and read new data
-#first, read ingestedfiles.txt
+#############Load config.json and get input and output paths
+with open('config.json','r') as f:
+    config = json.load(f) 
 
-#second, determine whether the source data folder has files that aren't listed in ingestedfiles.txt
+prod_folder = config['prod_deployment_path']
+source_data_folder = config['input_folder_path']
+data_path = config['output_folder_path']
 
-
-
-##################Deciding whether to proceed, part 1
-#if you found new data, you should proceed. otherwise, do end the process here
-
-
-##################Checking for model drift
-#check whether the score from the deployed model is different from the score from the model that uses the newest ingested data
-
-
-##################Deciding whether to proceed, part 2
-#if you found model drift, you should proceed. otherwise, do end the process here
-
-
-
-##################Re-deployment
-#if you found evidence for model drift, re-run the deployment.py script
-
-##################Diagnostics and reporting
-#run diagnostics.py and reporting.py for the re-deployed model
+def full_automation():
+    
+    logging.info("Checking if new data present and need to ingest")
+    old_data = []
+    with open(prod_folder + "/ingestedfiles.txt", 'r') as file:
+        for line in file:
+            old_data.append(line.strip())
+            
+    source_files = os.listdir(source_data_folder)
+    
+    if set(old_data) != set(source_files):
+        merge_multiple_dataframe()
+        
 
 
+    logging.info("checking for model drift")
+    logging.info("Predicting using old model")
+    with open(prod_folder + "/trainedmodel.pkl", 'rb') as file:
+        old_mopdel = pickle.load(file)
+    data = pd.read_csv(data_path + "/finaldata.csv")
+    X=data.loc[:,['lastmonth_activity','lastyear_activity', 'number_of_employees']].values.reshape(-1, 3)
+    y=data['exited'].values.reshape(-1, 1).ravel()
+    predicted = old_mopdel.predict(X)
+    f1score=metrics.f1_score(predicted,y)
+    
+    logging.info("Compare old score and new score and check for model drift")
+    with open(prod_folder + "/latestscore.txt", 'rb') as file:
+        old_score = float(file.readline().strip())
+    if old_score < f1score:
+        logging.info("MODEL DRIFT: start training new model")
+        train_model()
+        store_model_into_pickle()
+        score_model()
+        os.system("python diagnostics.py")
+        os.system("python apicalls.py")
+    else:
+        logging.info("Did not detect model drift")
+            
 
 
+
+if __name__ == '__main__':
+    full_automation()
 
 
 
